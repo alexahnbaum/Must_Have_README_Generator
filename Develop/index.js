8714f43ef2b82fbf518c289ef34e2db21eb767bb
Alex Baumgartner <aabaumgartner@gmail.com>
2024-02-23 14:29:44 -0800

    created mvp and uploading to github

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,89 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// Created a variable called license choices so that I can put a value on each license choice.  This will allow me to use the values in the license functions
+const licenseChoices = [
+  { name: "MIT License", value: "mit" },
+  { name: "Apache License 2.0", value: "apl" },
+  { name: "Mozilla Public License 2.0", value: "mpl" },
+  { name: "no license", value: "none" },
+];
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     message: "What is the name of your project?",
-    type: "text",
+    type: "input",
     name: "title",
   },
   {
-    message: "Descripe your project in 1-2 sentences.",
-    type: "text",
+    message: "Describe your project in 1-2 sentences.",
+    type: "input",
     name: "description",
+  },
+  {
+    message:
+      "Describe the installation procedure of your project in 1-2 sentences.",
+    type: "input",
+    name: "installation",
+  },
+  {
+    message:
+      "Describe how your project is intended to be used in 1-2 sentences.",
+    type: "input",
+    name: "usage",
+  },
+  {
+    message: "Describe the contribution guidelines in 1-2 sentences.",
+    type: "input",
+    name: "contribution",
+  },
+  {
+    message: "What are the testing instructions",
+    type: "input",
+    name: "testing",
+  },
+  {
+    message: "What license is the app covered under?",
+    type: "list",
+    name: "license",
+    choices: licenseChoices,
+  },
+  {
+    message: "What is your GitHub username?",
+    type: "input",
+    name: "GitHub",
+  },
+  {
+    message: "What is your email address?",
+    type: "input",
+    name: "email",
   },
 ];
 
-inquirer.prompt(questions);
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, generateMarkdown(data), (err) => {
+    err
+      ? console.log(`Error: ${err}`)
+      : console.log("high quality README saved");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+
+    .then((data) => {
+      console.log(data);
+      // const markDown = generateMarkdown(data);
+      const fileName = "HQ_README.md";
+      writeToFile(fileName, data);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,108 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  let licenseBadge = "";
+  // if else statement to give badge based on what license is picked
+  if (data.license === "mit") {
+    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (data.license === "apl") {
+    licenseBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (data.license === "mpl") {
+    licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  } else {
+    licenseBadge = "";
+  }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  let licenseURL = "";
+  // if else statement to give link based on what license is picked
+  if (data.license === "mit") {
+    licenseURL = "https://api.github.com/licenses/mit";
+  } else if (data.license === "apl") {
+    licenseURL = "https://api.github.com/licenses/apache-2.0";
+  } else if (data.license === "mpl") {
+    licenseURL = "https://api.github.com/licenses/mpl-2.0";
+  } else {
+    licenseURL = "";
+  }
+  return licenseURL;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  const badge = renderLicenseBadge(data);
+  const link = renderLicenseLink(data);
+  // if else statement to give license or an empy string if none
+  if (data.license === "mit" || "apl" || "mpl") {
+    return `${badge} ${link}`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${data.description}
 
+  ---
+  
+  **Table of contents**
+  
+  - [Installation](#item-one)
+  - [Usage](#item-two)
+  - [Contributing](#item-three)
+  - [Tests](#item-four)
+  - [License](#item-five)
+  - [Questions](#item-six)
+  
+  ---
+  
+  <a id="item-one"></a>
+  
+  ### INSTALLATION
+  
+  ${data.installation}
+  
+  <a id="item-two"></a>
+  
+  ### USAGE
+  
+  ${data.usage}
+  
+  <a id="item-three"></a>
+  
+  ### CONTRIBUTING
+  
+  ${data.contribution}
+  
+  <a id="item-four"></a>
+  
+  ### TESTS
+  
+  ${data.testing}
+  
+  <a id="item-five"></a>
+  
+  ### LICENSE
+  
+  ${renderLicenseSection(data)}
+  
+  <a id="item-six"></a>
+  
+  ### QUESTIONS
+  
+  If you have questions you can get in touch via the contacts below:
+
+  ${data.GitHub}
+
+  ${data.email}
 `;
 }
 
